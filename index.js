ebee548ba199b55fee42054dc62f2469c8095c39
mgorodilov <maksim.gorodilov@ah.nl>
2022-05-30 13:11:21 +0200

    chore: initial

---
+++ index.js
@@ -0,0 +1,43 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
+
+const typeDefs = gql`
+  type Book {
+      title: String
+      author: String
+  }
+  
+  type Query {
+      books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'Book 1',
+    author: 'Author',
+  },
+  {
+    title: 'Book 2',
+    author: 'Author'
+  }
+]
+
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: false,
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground()
+  ]
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Ready at ${url}`)
+})
